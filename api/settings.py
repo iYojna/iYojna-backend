f730f8ceb937bb166ffd540701c4caf9bb6ea137
mugdha273 <mugdhasharma0327@gmail.com>
2022-10-15 12:54:08 +0530

    adds swagger

--- api/settings.py
+++ api/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
     "administrator",
     "user",
     'rest_framework',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
