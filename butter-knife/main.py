103349cbbf2f7cf0e71ba82fc8e93501ef841b59
Phoenix <prodotzip@gmail.com>
2022-10-15 15:59:13 +0530

    scraped scheme v1

--- butter-knife/main.py
+++ butter-knife/main.py
@@ -1,9 +1,13 @@
 import json
+import os
+import re
 
 from bs4 import BeautifulSoup as soup
 from googletrans import Translator
 import requests
 
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 translator = Translator()
 ROOT_URL = "https://gueedc.gujarat.gov.in/"
@@ -24,15 +28,46 @@ def get_schemes():
     return schemes_dict
 
 
+def get_field(scheme_div, pattern):
+    scheme_field = [scheme_p for scheme_p in scheme_div.find_all("p") if
+                    pattern.search(_translate_data(scheme_p.text))]
+    if scheme_field:
+        scheme_field = scheme_field[0].text
+    else:
+        return
+    scheme_field = scheme_field.split(":")[-1]
+
+    return scheme_field
+
+
 def get_scheme_data(scheme_link):
-    scheme_data = json.load(open("scheme_data.json", "r"))
+    scheme_data = json.load(open(os.path.join(ROOT_DIR, "scheme_template.json"), "r"))
 
     response = requests.get(scheme_link)
 
     res_soup = soup(response.content, "html.parser")
     scheme_div = res_soup.find("div", {"class": "parishisht"})
 
-    return scheme_div
+    scheme_name = scheme_div.find("h2").text
+    scheme_name = scheme_name.split(":")[-1]
+    scheme_data["gu"]["name"] = scheme_name
+    scheme_data["en"]["name"] = _translate_data(scheme_name)
+
+    roi_pattern = re.compile(r"^rate of interest", re.IGNORECASE)
+    scheme_roi = get_field(scheme_div, roi_pattern)
+    if scheme_roi:
+        scheme_data["gu"]["interest_rate"] = scheme_roi
+        scheme_data["en"]["interest_rate"] = _translate_data(scheme_roi)
+
+    income_pattern = re.compile(r"^income", re.IGNORECASE)
+    scheme_income = get_field(scheme_div, income_pattern)
+    if scheme_income:
+        scheme_data["gu"]["income_limit"] = scheme_income
+        scheme_data["en"]["income_limit"] = _translate_data(scheme_income)
+
+    scheme_data["link"] = scheme_link
+
+    return scheme_data
 
 
 def _translate_data(data):
@@ -41,4 +76,5 @@ def _translate_data(data):
 
 
 schemes = get_schemes()
-
+test_scheme = list(schemes.values())[0]
+scheme_data = get_scheme_data(test_scheme)
