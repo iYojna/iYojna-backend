87ca5af0540357d708116dbe80b152be46cd6a52
mugdha273 <mugdhasharma0327@gmail.com>
2022-10-15 19:39:23 +0530

    minor update

--- schemes/admin.py
+++ schemes/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import GujSchemeModel,EnglishSchemeModel
 # Register your models here.
+admin.site.register(GujSchemeModel)
+admin.site.register(EnglishSchemeModel)
