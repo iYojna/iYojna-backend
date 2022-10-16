3178d9cd4cf3ee756532ee8264e6d8f4939344b4
Riya Parikh <73838328+RiyaParikh0112@users.noreply.github.com>
2022-10-16 08:17:55 +0530

    stemming and stop words removed

--- utils/get-tags/get-tags.py
+++ utils/get-tags/get-tags.py
@@ -1,6 +1,31 @@
 import model
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
+def remove_stopwords(inputStr):
+    new_text = []
+    
+    for word in inputStr.split():
+        if word in stopwords.words('english'):
+            new_text.append('')
+        else:
+            new_text.append(word)
+    x = new_text[:]
+    new_text.clear()
+    return " ".join(x)
+
+ps = PorterStemmer()
+def stem_words(inputStr):
+    return " ".join([ps.stem(word) for word in inputStr.split()])
+
 
 def getTags(inputStr):
     tags = model.nlp(inputStr)
     return tags
+
+
+def nltk():
+  return  remove_stopwords(),stem_words(), getTags()
+    
+
     
