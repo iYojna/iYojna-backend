5e42eda67e985a93ae906ba49c1f2563fda6682f
Phoenix <prodotzip@gmail.com>
2022-10-16 10:32:05 +0530

    Gujarati support

--- schemes/views.py
+++ schemes/views.py
@@ -8,13 +8,27 @@ from utils.db.utils import update_schemes, is_eligible
 from utils.get_tags.get_tags import getTags
 
 from .models import EnglishSchemeModel, GujSchemeModel
-from .serializers import EnglishSchemeModelSerializer
+from .serializers import EnglishSchemeModelSerializer, GujSchemeModelSerializer
 
 # Create your views here.
 
 ls = ["agricultre", "cooperation", "education", "civil", "home", "administration", "mines", "labour", "tribal", "urban",
       "women", "child", "ITC", "BSF", "NTPC", "Toy", "innovation", "collector", "DDO", "municipal", "science",
       "technology"]
+
+
+def get_model_by_request(request):
+    if request.GET.get("lang") == "gu":
+        return GujSchemeModel
+    else:
+        return EnglishSchemeModel
+
+
+def get_serializer_by_request(request):
+    if request.GET.get("lang") == "gu":
+        return GujSchemeModelSerializer
+    else:
+        return EnglishSchemeModelSerializer
 
 
 class UpdateSchemeView(views.APIView):
@@ -27,14 +41,14 @@ class SchemesView(views.APIView):
     queryset = EnglishSchemeModel.objects.all()
 
     def get(self, request):
-        queryset = EnglishSchemeModel.objects.all()
-        serializer = EnglishSchemeModelSerializer(queryset, many=True)
+        queryset = get_model_by_request(request).objects.all()
+        serializer = get_serializer_by_request(request)(queryset, many=True)
         return Response(serializer.data)
 
 
 class UpdateSchemeTagsView(views.APIView):
     def get(self, request):
-        schemes = EnglishSchemeModel.objects.all()
+        schemes = get_model_by_request(request).objects.all()
         for x in schemes:
             name = x.name
             desc = x.desc
@@ -60,7 +74,7 @@ class RetTagSchemeView(views.APIView):
         if tag is None:
             return Response({"message": "Please provide a tag"}, status=status.HTTP_400_BAD_REQUEST)
         tag = request.query_params.get('tag', None).lower()
-        schemes = EnglishSchemeModel.objects.all()
+        schemes = get_model_by_request(request).objects.all()
         resp = {}
         final_resp = {}
         for x in schemes:
@@ -69,8 +83,8 @@ class RetTagSchemeView(views.APIView):
             # print(strls)
             for y in strls:
                 if y == str(tag):
-                    x_scheme = EnglishSchemeModel.objects.get(id=x.id)
-                    x_scheme_data = EnglishSchemeModelSerializer(x_scheme).data
+                    x_scheme = get_model_by_request(request).objects.get(id=x.id)
+                    x_scheme_data = get_serializer_by_request(request)(x_scheme).data
                     resp[x.id] = x_scheme_data
 
         for scheme_id, scheme_data in resp.items():
@@ -89,7 +103,7 @@ class RetQuerySchemeView(views.APIView):
         if query is None:
             return Response({"message": "Please provide a query"}, status=status.HTTP_400_BAD_REQUEST)
         query = query.lower().split(",")
-        schemes = EnglishSchemeModel.objects.all()
+        schemes = get_model_by_request(request).objects.all()
         resp = OrderedDict()
 
         for x in schemes:
@@ -102,7 +116,7 @@ class RetQuerySchemeView(views.APIView):
                 if quer in strls:
                     num += 1
             if num != 0:
-                x_scheme_data = EnglishSchemeModelSerializer(x).data
+                x_scheme_data = get_serializer_by_request(request)(x).data
                 if resp.get(num / len(query)):
                     resp[num / len(query)].append(x_scheme_data)
                 else:
